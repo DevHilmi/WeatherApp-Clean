573e88474074a61ccedaf3ee9dc5d60705934db6
hilmi.rizaldi <developer.hilmi@gmail.com>
2019-01-08 17:25:50 +0700

    Initial Android Project

---
+++ app/src/androidTest/java/com/rizaldev/weatherapp/ExampleInstrumentedTest.java
@@ -0,0 +1,27 @@
+package com.rizaldev.weatherapp;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.rizaldev.weatherapp", appContext.getPackageName());
+    }
+}
